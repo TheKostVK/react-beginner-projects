c2cbbbe1fd024aff9fdc3b709b9ea06fad140641
TheKost_ <thekost2@yandex.ru>
2023-04-15 22:01:51 +0300

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './index.scss';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const plus = () => {
+        setCount(count + 1);
+    }
+
+    const minus = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={() => minus()}>- Минус</button>
+                <button className="plus" onClick={() => plus()}>Плюс +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
